b334686d615b9975938ce2af6be650f4a6116d8a
K@malG@rg20 <51014441+rare20@users.noreply.github.com>
2024-12-19 18:07:22 +0000

    add datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3+3) we are using node js not the browser
+//code readability achi honi chahiye in any language
+let name = "kamal"
+let age= 20
+
+
+//number = 2 to power 53
+//string => ""
+//bigint 
+//boolean => true/false
+//null => standalone value
+//undefined 
+//symbol =>unique
+//object
+console.log(typeof null);
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof undefined);
