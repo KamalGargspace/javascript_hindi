996952dd7f0439238bf8a58f5decda153dcacf88
K@malG@rg20 <51014441+rare20@users.noreply.github.com>
2024-12-19 17:43:01 +0000

    variables done

---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId=123445
+let accountEmail="kamal@gmail.com"
+var accountPass = "4567"
+let accountState;
+//accountId = 5677 //not allowed
+
+accountEmail = "haca@gmail.com"
+accountPass = "32453"
+accountCity ="bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
