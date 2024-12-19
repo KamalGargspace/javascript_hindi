cb4268c25ca23f59182c3bfb920737930819e1df
K@malG@rg20 <51014441+rare20@users.noreply.github.com>
2024-12-19 18:23:21 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperator.js
@@ -0,0 +1,24 @@
+let score = "33a";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinNumber = Number(score);
+console.log(typeof valueinNumber);
+console.log(valueinNumber);
+
+//33 => 33
+//33abc => NaN
+// true =>1 and false=> 0 
+
+let isBool= 1
+let isBoolinboolean=Boolean(isBool);
+
+console.log(isBoolinboolean);
+// 1=> true
+//0=> false
+// string=> true
+
+
+
+
