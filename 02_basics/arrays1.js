c059d8a1932b5ac63a0b84304b712e4b5de48ae9
K@malG@rg20 <51014441+rare20@users.noreply.github.com>
2024-12-25 18:58:19 +0000

    add arrays part 1

---
+++ 02_basics/arrays1.js
@@ -0,0 +1,47 @@
+//array
+
+const arr = [1,2,3,445,342,23]//can have distinct datatypes elements and can be expand in the js
+
+console.log(arr[0]);
+const Heroes = ["iron man","batman","captain"]
+
+const arr2 = new Array(1,2,3,4)
+console.log(arr2[1]);
+
+//Array methods
+
+// arr.push(6)
+// arr.push(7)
+// arr.pop()
+// arr.unshift(9)
+// console.log(arr);
+
+// arr.shift()
+// console.log(arr);
+
+// console.log(arr.includes(9));
+// console.log(arr.indexOf(2));
+
+// const newarr = arr.join()
+// console.log(arr);
+// console.log(newarr);
+
+// slice and spice
+
+console.log("A ", arr);
+const myn1 = arr.slice(1,4)
+console.log(myn1);
+console.log("B",arr);
+
+const myn2 = arr.splice(1,4)
+console.log("C",arr);
+console.log(myn2);
+
+
+
+
+
+
+
+
+
