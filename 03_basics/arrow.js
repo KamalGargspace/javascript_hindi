1b1f8a39f33c9bda78d3be7b7044ee8e333e5242
Kamal Garg <51014441+KamalGargspace@users.noreply.github.com>
2024-12-28 17:56:35 +0000

    arrow

---
+++ 03_basics/arrow.js
@@ -0,0 +1,56 @@
+const user = {
+    username: "kamal",
+    price: 199,
+    welcomeMessage: function(){
+        console.log(`${this.username},welcome to the website`);
+        // console.log(this);
+        
+        
+    }
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "kamal"
+//     console.log(this.username);
+    
+//     console.log(this);
+    
+// }
+// chai()
+//  const chai = function chai(){
+//         let username = "kamal"
+//         console.log(this.username);
+        
+//         // console.log(this);
+        
+//     }
+//     chai()
+
+    // const chai = () => {
+    //     let username = "kamal"
+    //     console.log(this.username);
+        
+    //     // console.log(this);
+        
+    // }
+    // chai()
+
+// const addTwo = (num1,num2) => {
+//     return num1+num2
+// }
+// const addTwo = (num1,num2) => (num1 + num2)
+// const addTwo = (num1,num2) => num1 + num2// implicit trika h
+
+const addTwo = (num1,num2) => ({username:"kamal"})
+
+console.log(addTwo(3,4));
+
+// const myarr =[2,444,23,45]
+
+// myarr.forEach()
