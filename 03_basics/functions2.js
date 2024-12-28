5546a08f67035f8ffea1aa722447c21942b85ede
Kamal Garg <51014441+KamalGargspace@users.noreply.github.com>
2024-12-28 17:01:53 +0000

    functions2

---
+++ 03_basics/functions2.js
@@ -0,0 +1,30 @@
+function calculateCartPrice(val1,val2,...num1){//rest operator for array bta rha {...}
+ return num1
+}
+
+// console.log(calculateCartPrice(200,300,400,500));
+const user = {
+    username : "kamal",
+    price: 199
+}
+
+function handleobject(anyobject){
+   console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleobject(user)
+handleobject({
+    username: "garg",
+    price: 299
+})
+
+const myNewArray = [200,300,100,299]
+function returnsecondvalue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnsecondvalue(myNewArray));
+console.log(returnsecondvalue([200,32,3423,43234]));
+
+
+
